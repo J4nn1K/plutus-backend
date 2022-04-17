3fff15bc1054246ef9b4958ea2f5e23d3e12602a
J4nn1K <jannikjorgegrothusen@outlook.de>
2022-04-17 22:18:51 +0200

    Remove CORS options

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ const db = require("./app/models");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
-app.use(cors(corsOptions));
+// var corsOptions = {
+//   origin: "http://localhost:3000"
+// };
+// app.use(cors(corsOptions));
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+// //   res.header("Access-Control-Allow-Headers", "*");
+// //   res.header("Access-Control-Allow-Methods", "*");
+// //   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
